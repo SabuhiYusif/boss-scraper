72716029411c8c7cbc1030102614bca8590a40f0
Sabuhi Yusifov <sayusi@ttu.ee>
2018-11-08 02:05:32 +0200

    making method get_links that takes link of the page

--- scrape.py
+++ scrape.py
@@ -1,19 +1,23 @@
 from urllib.request import urlopen
 from bs4 import BeautifulSoup
 
+
+def get_links(page_link):
+    
+  page = urlopen(page_link)
+  soup = BeautifulSoup(page, 'html.parser')
+
+
+  name_box = soup.find_all('div', attrs={'class': 'results-i'})
+
+  data = []
+  for i in name_box:
+      if i.text.__contains__("PROQRAM"):
+        link = i.find('a', attrs={'class': 'results-i-link'})
+        data.append( 'https://boss.az' + link['href'])
+
+
+  print(data)
 quote_page = 'https://boss.az/vacancies?action=index&controller=vacancies&only_path=true&search%5Bcategory_id%5D=38&type=vacancies'
 
-page = urlopen(quote_page)
-soup = BeautifulSoup(page, 'html.parser')
-
-
-name_box = soup.find_all('div', attrs={'class': 'results-i'})
-
-data = []
-for i in name_box:
-    if i.text.__contains__("PROQRAM"):
-      link = i.find('a', attrs={'class': 'results-i-link'})
-      data.append( 'https://boss.az' + link['href'])
-
-
-print(data)
+get_links(quote_page)
